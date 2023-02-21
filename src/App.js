e5b4042b44933043b531b15724e77a75c6f28363
hoai1506 <luuhoai3107@gmail.com>
2023-02-21 10:48:33 +0700

    fc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  createBrowserRouter, RouterProvider, useSearchParams, createSearchParams
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+]);
+
+
 
 function App() {
+  const [queryParam, setQueryParam] = useSearchParams();
+  const value = queryParam.get('type')
+
+setQueryParam(
+  createSearchParams({
+      frunt: 'hello',
+      xinchao: 'hi'
+  }))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <RouterProvider
+    router={router}
+   />
+  )
+ 
 }
 
 export default App;
